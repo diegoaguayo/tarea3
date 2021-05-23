ae314b0f5cb98c03168796a06904ae54b59d68e1
diegoaguayo <dtaguayo@uc.cl>
2021-05-23 03:30:05 -0400

    added build for production

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Cambie la pagina
         </p>
         <a
           className="App-link"
