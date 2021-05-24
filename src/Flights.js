05fb1ced5c8158783a63cfb02c0e77218ff0ba72
diegoaguayo <dtaguayo@uc.cl>
2021-05-24 10:15:11 -0400

    added passengers visualization

--- src/Flights.js
+++ src/Flights.js
@@ -24,9 +24,17 @@ export default function Flights() {
 
   const flightList = flights.map((flight) =>
         <li>
-            <ShowFlight flight={flight}/>
+            <ShowFlight flight={flight} passengerList={passengerList(flight.passengers)}/>
         </li>
     );
+
+  function passengerList(passengers) {
+    return passengers.map((p) =>
+    <ShowPassenger passenger={p}/>
+);
+  }
+
+  
 
   return (
     <div>
@@ -42,7 +50,7 @@ export default function Flights() {
   );
 }
 
-const ShowFlight = ({flight}) => (
+const ShowFlight = ({flight, passengerList}) => (
   <Popup trigger={<button> {flight.code}</button>} position="right center">
     <div>
       <h3>INFORMACIÓN DEL VUELO</h3>
@@ -51,9 +59,26 @@ const ShowFlight = ({flight}) => (
       <p>Origin: {flight.origin}</p>
       <p>Destination: {flight.destination}</p>
       <p>Number of seats: {flight.seats}</p>
+      <ShowPassengers passengers={passengerList}/>
     </div>
   </Popup>
 );
+
+const ShowPassengers = ({passengers}) => (
+  <Popup trigger={<button> Passengers</button>} position="right center">
+    <div>
+      <h4>Pasajeros</h4>
+      {passengers}
+    </div>
+  </Popup>
+);
+
+const ShowPassenger = ({passenger}) => (
+    <p>
+      Nombre: {passenger.name}, Edad: {passenger.age}
+    </p>
+);
+
 
 
 
