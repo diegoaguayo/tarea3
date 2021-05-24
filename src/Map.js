e51c4b4417757c3c9eeb433e680b4cf494dacc20
diegoaguayo <dtaguayo@uc.cl>
2021-05-24 08:34:33 -0400

    improved map visuals

--- src/Map.js
+++ src/Map.js
@@ -25,8 +25,13 @@ export default function Map({flights}) {
 
   function logPositions(data) {
     setPositionPoints( oldArray => [...oldArray, data])
-    airplanePositions[data.code] = data;
-    console.log(airplanePositions)
+    var angle = 0;
+    if (airplanePositions[data.code]) {
+      const last_pos = airplanePositions[data.code].position
+      const new_pos = data.position
+      var angleDeg = Math.atan2(last_pos[0] - new_pos[0], last_pos[1] - new_pos[1]) * 180 / Math.PI;
+    }
+    airplanePositions[data.code] = {...data, angle: angleDeg};
     setAirplanePositions({...airplanePositions});
   }
 
@@ -71,20 +76,19 @@ function Displacement({flight}) {
   }
 
 function PaintPosition({flight}) {
-  const fillRedOptions = { fillColor: 'black' };
   return (
-    <Circle center={flight.position} pathOptions={fillRedOptions} radius={200} />
+    <Circle center={flight.position} color="grey" radius={200} />
   )
 }
 
 function DisplayAirplane({airplane}) {
 
-  const size = 50
+  const size = 35
 
   const myIcon = L.icon({
     iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/c/c2/20_airtransportation.svg',
     iconSize  : [size, size],
-    iconAnchor: [size/2, size + 9],
+    iconAnchor: [size/2, size/2],
   });
 
   return (
